10c4b3b84c7eca812d0e2ec283370d0e9cc4f85b
Mohammad Mehdi Nazari <75282167+MMNazari1380@users.noreply.github.com>
2024-01-05 19:28:29 +0330

    first working version

--- js/script.js
+++ js/script.js
@@ -1,50 +1,79 @@
-/*
-function handleSubmit(event){
-    document.getElementById('myForm').addEventListener('submit', function(e) {
-        e.preventDefault();
-        const formData = new FormData(this);
-        const textInput = formData.get('textInput');
-        //const apiOption = formData.get('apiOption');
-      
-        fetch('https://api.genderize.io/?name=' + textInput, {
-            method: "GET"
-        }) //+ '&option=' + apiOption)
-          .then(response => {
-            
-            if (!response.ok) {
-              throw new Error('Network response was not ok');
-            }
-            return response.json();
-          })
-          .then(data => {
-            const responseElement = document.getElementById('response');
-            responseElement.innerHTML = JSON.stringify(data);
-          })
-          .then(html => this.innerHTML)
-          .catch(error => console.error('Error:', error));
-          
-      });
+
+function handleSave(){
+
+  let textInput = document.getElementById('textInput').value;
+  let paragraphInput = document.getElementById('response gender');
+  let radioMale = document.getElementById('male radio').checked;
+  let radioFemale = document.getElementById('female radio').checked;
+  
+  if(paragraphInput.textContent == ""){
+    if (radioMale== true){
+      localStorage.setItem(textInput.value, "male");
+    }
+    else if (radioFemale == true){
+      localStorage.setItem(textInput.value, "female");
+    }
+  }
+  else {
+    localStorage.setItem(textInput.value, paragraphInput.textContent);
+  }
+
+  savedStoage();
 }
-*/
+
+function handleClear(){
+  localStorage.clear();
+  document.getElementById("saved").innerText = "Local storage has been cleared";
+}
 
 function handleSubmit(event) {
   event.preventDefault();
-  const textInput = document.getElementById('textInput').value;
+  let textInput = document.getElementById('textInput').value;
+  let radioMale = document.getElementById('male radio').checked;
+  let radioFemale = document.getElementById('female radio').checked;
 
-  fetch('https://api.genderize.io/?name=' + textInput, {
-    method: "GET"
-  })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json();
+  
+    fetch('https://api.genderize.io/?name=' + textInput, {
+      method: "GET"
     })
-    .then(data => {
-      const responseElement = document.getElementById('response');
-      responseElement.innerHTML = JSON.stringify(data);
-    })
-    .catch(error => console.error('Error:', error));
-}
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        const responseElementGender = document.getElementById('response gender');
+        responseElementGender.innerHTML = JSON.stringify(data.gender);
+  
+        const responseElementProbability = document.getElementById('response probability');
+        responseElementProbability.innerHTML = JSON.stringify(data.probability);
+      })
+      .catch(error => console.error('Error:', error));
+
+  }
+
+
+  
 
 document.getElementById('myForm').addEventListener('submit', handleSubmit);
+
+function savedStoage(){
+  let textInput = document.getElementById('textInput').value;
+  // Check if local storage is supported
+  if (typeof(Storage) !== "undefined") {
+  // Retrieve the item from local storage
+  var savedItem = localStorage.getItem(textInput.value);
+
+  // Display the item
+  if (savedItem) {
+    document.getElementById("saved").innerText = savedItem;
+  } else {
+    document.getElementById("saved").innerText = "No item found in local storage.";
+  }
+  } else {
+    document.getElementById("saved").innerText = "Local storage is not supported.";
+  }
+
+}
+
